d88d9123430f0d93f4075814e1eb13ab0115f7d4
Bobo <bnbsking@gmail.com>
2020-07-13 09:55:29 +0800

    Add files via upload

---
+++ argparse_.py
@@ -0,0 +1,8 @@
+import argparse, os
+
+parser = argparse.ArgumentParser(description="info. before args", epilog="info. after args")
+parser.add_argument('--foo', help='This is foo arg')
+parser.add_argument('-n', '--name', default="bobo", help='This is name arg')
+arg = parser.parse_args() # show in cmd / arg.xxx (--xxx only)
+
+print( "Hello {}".format(arg.foo) )
