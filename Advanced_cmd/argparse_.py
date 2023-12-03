04a6fe329af9c4a3e6d7bf5a415d2a0a9eee92ba
jameschao <jameschao@MacBook-Pro.local>
2023-12-03 21:15:17 +0800

    mod

--- Advanced_cmd/argparse_.py
+++
@@ -1,8 +0,0 @@
-import argparse, os
-
-parser = argparse.ArgumentParser(description="info. before args", epilog="info. after args", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--foo', help='This is foo arg')
-parser.add_argument('-n', '--name', default="bobo", help='This is name arg')
-arg = parser.parse_args() # show in cmd / arg.xxx (--xxx only)
-
-print( "Hello {}".format(arg.foo) )
--- Advanced_cmd/chr_ord_48_65_97.py
+++
@@ -1,5 +0,0 @@
-# chr # int -> chr
-# ord # chr -> int
-# chr(48) = '0'; ord('0') = 48
-# chr(65) = 'A'; ord('A') = 65
-# chr(97) = 'a'; ord('a') = 97
